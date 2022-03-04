228591484c3e427cf637ac4bedbfcd9bb12dc26b
Tsurara <fukagawa.sat1009@gmail.com>
2022-03-04 15:46:57 +0900

    [Add]veuOrderDetail

---
+++ backend/resources/js/components/orderDetail/PaymentMethod.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="payment">
+        <input type="radio" id="credit" name="" value="credit" v-model="select">クレジット支払い
+        <input type="radio" id="cash" name="" value="cash" v-model="select">代引き
+    </div>
+</template>
